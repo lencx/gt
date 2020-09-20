9694e81337a0ebc00c3713ce53884ac297e63356
lencx <cxin1314@gmail.com>
2020-09-21 00:35:22 +0800

    chore: github request

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 > github trending command line tool.
 
+## Dependencies
+
+| Tag | Description |
+| --- | ----------- |
+| <?> | may be used |
+| <✓> | use         |
+
+* [<?> Cursive](https://github.com/gyscos/Cursive)
+* [<?> prettytable-rs](https://github.com/phsym/prettytable-rs)
+* [<?> image](https://github.com/image-rs/image)
+
 ---
 
 [MIT](https://opensource.org/licenses/MIT) © lencx
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
+// use reqwest::header::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
+    // let resp = reqwest::get("https://httpbin.org/ip")
+    let params = [
+        ("q", "created:2020-09-18T00:00:00%2B08:00..2020-09-20T00:00:00%2B08:00"),
+        ("sort", "stars"),
+        ("order", "desc"),
+    ];
+    // let mut headers = HeaderMap::new();
+    // headers.insert(CONTENT_TYPE, "application/json; charset=utf-8".parse().unwrap());
+    let client = reqwest::Client::new();
+    let resp = client.get("https://api.github.com/search/repositories")
+        .query(&params)
+        .send()
         .await?;
+        // .json::<HashMap<String, String>>()
+        // .await?;
     println!("{:#?}", resp);
     Ok(())
 }
